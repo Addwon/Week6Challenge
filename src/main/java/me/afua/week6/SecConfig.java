255621dd0ab1f91a7a6b7f72e4abcc09fbe00021
aoa4eva <hi@aoa4eva.me>
2018-03-02 06:56:40 -0500

    Added BCrypt password encoder (password encoder required by Spring Boot2)

--- src/main/java/me/afua/week6/SecConfig.java
+++ src/main/java/me/afua/week6/SecConfig.java
@@ -1,25 +1,35 @@
 package me.afua.week6;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
 public class SecConfig extends WebSecurityConfigurerAdapter{
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("username").password("password").authorities("USER")
-                .and().withUser("admin").password("password").authorities("ADMIN");
+
+        PasswordEncoder pE = passwordEncoder();
+        auth.inMemoryAuthentication().withUser("username").password(pE.encode("password")).authorities("USER")
+                .and().withUser("admin").password(pE.encode("password")).authorities("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated()
         .and()
-        .formLogin()
+        .formLogin().permitAll()
         .and()
         .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
     }
